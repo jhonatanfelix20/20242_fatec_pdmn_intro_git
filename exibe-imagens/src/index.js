767956a9e284a0002ce2e26c6234bbd8eef58ecc
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-09-21 23:46:13 -0300

    feat(exibe-imagens): implementando as classes

---
+++ exibe-imagens/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'primereact/resources/primereact.min.css'
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
